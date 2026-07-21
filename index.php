c917830bf1df2d5fa85aa0cfbf35e3748d8609fc
ChitranshBP <chitransh@brandingpioneers.co.in>
2026-07-21 17:23:28 +0530

    all done ruttle chnages mae

--- index.php
+++ index.php
@@ -274,7 +274,7 @@ $page_description = "Tranquil Minds Mental Health: A sanctuary for mental wellne
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 9.172V5L8 4z"></path>
                                     </svg>
                                 </div>
-                                <h3 class="text-lg font-bold text-primary mb-2" style="font-family: 'Bauhaus Soft', cursive;">Medication Mgmt</h3>
+                                <h3 class="text-lg font-bold text-primary mb-2" style="font-family: 'Bauhaus Soft', cursive;">Medication Management</h3>
                                 <p class="text-gray-600 text-xs leading-relaxed font-normal">
                                     We integrate medication management to supervise and optimize psychiatric treatments alongside your therapy.
                                 </p>
